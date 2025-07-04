82baf4b0bee1f3bade2039613deb554e50411fac
priyanshu kumar <114394901+priyanshu-rj@users.noreply.github.com>
2025-07-05 01:45:26 +0530

    Update Food.jsx

--- frontend/src/component/food/Food.jsx
+++ frontend/src/component/food/Food.jsx
@@ -1,30 +1,24 @@
-import React, { useContext } from 'react';
-import './food.css';
-import { StoreContext } from '../../context/Scontext';
-import Fooditem from '../fooditem/Fooditem';
+import React, { useContext } from 'react'
+import './food.css'
+import { StoreContext } from '../../context/Scontext'
+import Fooditem from '../fooditem/Fooditem'
+const Food = (category) => {
 
-const Food = () => {
-  const { food_list } = useContext(StoreContext);
+    const {food_list} = useContext(StoreContext) 
+
 
   return (
-    <div className='food-display' id='food-display'>
-      <h2>Top Products near you</h2>
-      <div className="food-display-list">
+    <div className='food-display' id='food-display' >
+       <h2>Top Prdoduct near you</h2>
+       <div className="food-display-list">
         {
-          food_list.map((item, index) => (
-            <Fooditem
-              key={index}
-              id={item._id}
-              name={item.name}
-              description={item.description}  
-              price={item.price}
-              image={item.image} 
-            />
-          ))
+            food_list.map((item,index)=>{
+               return <Fooditem key={index} id={item._id} name={item.name} descrption={item.descrption} price={item.price} image={item.image}/>
+            })
         }
-      </div>
+       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Food;
+export default Food
