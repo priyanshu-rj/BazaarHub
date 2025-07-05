04a1c5650f753982bdd964fca13913fcf4a2c170
priyanshu kumar <114394901+priyanshu-rj@users.noreply.github.com>
2025-07-05 17:28:16 +0530

    Update Navbar.jsx

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../../assets/assets'
 const Navbar = () => {
   return (
     <div className='navbar'>
-       <img className='logo' src={assets.logo} alt="" />
+       <img className='logo' src={assets.logo1} alt="" />
        <h2>Admin Portal</h2>
         <img className='profile' src={assets.profile_image} alt="" />
     </div>
